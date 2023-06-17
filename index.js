8cea6a0cb4e7609bc02cc1b45728290f9dfdfd26
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-06-17 12:16:05 -0600

    Creates 3 classes!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup,drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.#dessert = dessert;
+      this.entree = entree;
+    }
+}
+
+let am = new Breakfast("pancakes", "milk")
+let noon = new Lunch("Tossed", "Minnestrone", "Coke")
+let evening = new Dinner("Tossed", "Wonton", "Ribeye", "Ice Cream Cake")
+
+console.log(am, noon, evening)
+
